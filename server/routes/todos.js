7ec662bfa9dacedf90eed85c27d4553efda6d218
unknown <alokkumar991992@gmail.com>
2022-05-04 02:01:51 +0530

    todos router working

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,4 +2,15 @@
 const express = require('express');
 let router =  express.Router();
 
+router.get('/cars', (req, res) => {
+    res.send('this is things car')
+  });
+  
+  router.post('/cars', function (req, res) {
+    res.send('POST request to the homepage')
+  });
+
+
+
 module.exports = router;
+
