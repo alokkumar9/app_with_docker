7be9bcb44579542b3345a2fdb1f4bad2dea7ad10
unknown <alokkumar991992@gmail.com>
2022-05-04 02:15:24 +0530

    todos jason response coming

--- server/app.js
+++ server/app.js
@@ -3,19 +3,12 @@ const app = express();
 const port =process.env.port||3000;
 const todos = require("./routes/todos");
 
-app.use('/things',todos);
+app.use('/api/todos',todos);
 
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
-});
+// app.get('/', (req, res) => {
+//   res.send('GET request to the homepage')
+// });
 
-app.get('/things/cars', (req, res) => {
-  res.send('things car');
-});
-
-app.post('/things/cars', function (req, res) {
-  res.send('POST request to the homepage')
-});
 
 app.listen(port,err => {
 if(err){
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,16 +1,39 @@
-'use strict';
-const express = require('express');
-let router =  express.Router();
+// 'use strict';
+// const express = require('express');
+// let router =  express.Router();
 
-router.get('/cars', (req, res) => {
-    res.send('this is things car')
-  });
+// router.get('/cars', (req, res) => {
+//     res.send('this is things car')
+//   });
   
-  router.post('/cars', function (req, res) {
-    res.send('POST request to the homepage')
-  });
+//   router.post('/cars', function (req, res) {
+//     res.send('POST request to the homepage')
+//   });
 
 
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+
+const TODOS = [
+  { title: 'Create API to get this list', isDone: true },
+  { title: 'Connect API with Angular', isDone: true },
+  { title: 'Connect server with mongo', isDone: false },
+  { title: 'Publish app', isDone: false },
+];
+
+/* GET /api/todos */
+router.get('/', async (req, res) => {
+  try {
+    res.json(TODOS);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
+
+
+
 
